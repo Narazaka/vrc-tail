142b00ca5953ca9cb9b4c4ba5ecea0b7cb2bf980
Narazaka <info@narazaka.net>
2024-11-02 14:47:38 +0900

    fix gen manifest

--- scripts/makeManifest.mjs
+++ scripts/makeManifest.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
 import { sha256sum } from "./sha256sum.mjs";
-import fs from "fs/promises";
+import fs, { promises as fsp } from "fs";
 import { join } from "path";
 
 const basedir = join(import.meta.dirname, "..");
 
 const packageVersion = JSON.parse(
-  await fs.readFile(join(basedir, "package.json"), "utf8"),
+  await fsp.readFile(join(basedir, "package.json"), "utf8"),
 ).version;
 const packageHash = await sha256sum(join(basedir, "vrc-tail.exe"));
 
@@ -18,17 +18,20 @@ function replaceManifestStrings(content) {
 }
 
 async function replaceManifest(filename) {
-  const content = await fs.readFile(
+  const content = await fsp.readFile(
     join(basedir, "manifest/winget", filename),
     "utf8",
   );
   const replaced = replaceManifestStrings(content);
-  await fs.writeFile(
+  await fsp.writeFile(
     join(basedir, "manifest/winget/generated", filename),
     replaced,
   );
 }
 
+if (!fs.existsSync(join(basedir, "manifest/winget/generated"))) {
+  await fsp.mkdir(join(basedir, "manifest/winget/generated"));
+}
 await replaceManifest("Narazaka.vrc-tail.yaml");
 await replaceManifest("Narazaka.vrc-tail.installer.yaml");
 await replaceManifest("Narazaka.vrc-tail.locale.en-US.yaml");
